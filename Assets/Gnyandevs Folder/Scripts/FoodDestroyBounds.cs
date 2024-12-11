e8fce54a88610a81395c5383cefe33a35722f138
Jules <jup1439@thi.de>
2024-12-11 01:12:28 +0100

    Gnyandevs changes with some issues
    - whole asset folder from ar foundation samples 5.1 is now in
    - issues with XRInteractionTools: This project uses 3.0.4 and the previous one with face tracking is using 2.3.1)

---
+++ Assets/Gnyandevs Folder/Scripts/FoodDestroyBounds.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodDestroyBounds : MonoBehaviour
+{
+    public float outOfBoundsY=2.0f;
+    void Update()
+    {
+        if (transform.position.y < Camera.main.transform.position.y - outOfBoundsY)
+        {
+            Destroy(gameObject);
+            Debug.Log("Food missed!");
+        }
+    }
+}
